f21562c318f55ff42bbc5ef070fc8a4234a4dfeb
mbwilding <mbwilding@gmail.com>
2021-11-21 21:15:10 +0800

    Updater fixed

--- Djenerative/App.xaml.cs
+++ Djenerative/App.xaml.cs
@@ -2,8 +2,13 @@
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
 using System.Windows;
+using AdonisUI.Controls;
+using MessageBox = AdonisUI.Controls.MessageBox;
+using MessageBoxImage = AdonisUI.Controls.MessageBoxImage;
+using MessageBoxResult = AdonisUI.Controls.MessageBoxResult;
 
 namespace Djenerative
 {
@@ -12,44 +17,55 @@ namespace Djenerative
     /// </summary>
     public partial class App : Application
     {
+        private const string UpdateForce = "update.txt";
         private const string VersionLink = "https://onedrive.live.com/download?cid=B0EF6D8226FA4E78&resid=B0EF6D8226FA4E78%21221726&authkey=ACSiUEwsBAZph3I";
         private const string DownloadLink = "https://onedrive.live.com/download?cid=B0EF6D8226FA4E78&resid=B0EF6D8226FA4E78%21221716&authkey=AP5XrkWUi_uQCRo";
+        private readonly HttpClient _client = new();
 
         public App()
         {
         }
 
-        private void Update()
+        private async Task WebShit(string url)
+        {
+            // best practice to create one HttpClient per Application and inject it
+            
+
+            using HttpResponseMessage response = await _client.GetAsync(url);
+            using HttpContent content = response.Content;
+            var json = await content.ReadAsStringAsync();
+        }
+
+        private void UpdateCheck()
         {
             try
             {
-                string version = new WebClient().DownloadString(VersionLink).Replace("\\r\\n", "").Replace("\\n", "").Trim();
+                using HttpResponseMessage response = _client.GetAsync(VersionLink).Result;
+                using HttpContent content = response.Content;
+                string result = content.ReadAsStringAsync().Result;
+                string version = result.Replace("\\r\\n", "").Replace("\\n", "").Trim();
+
+                if (version == "0")
+                {
+                    Environment.FailFast("Disabled");
+                }
+
                 bool success = int.TryParse(version, out int _);
                 if (!success)
                 {
                     Environment.FailFast("No Internet");
                 }
+
+                if (File.Exists(UpdateForce))
+                {
+                    File.Delete(UpdateForce);
+                    Update(version);
+                    return;
+                }
+
                 if (Djenerative.Properties.Preset.Default.Version == int.Parse(version)) return;
 
-                string download = Path.Combine(Environment.CurrentDirectory, "update.tmp");
-                new WebClient().DownloadFile(DownloadLink, download);
-
-                var process = Process.GetCurrentProcess();
-                string fullPath = process.MainModule!.FileName!;
-                string exe = Path.GetFileName(fullPath);
-                string tmp = Path.GetFileName(download);
-
-                Djenerative.Properties.Preset.Default.Version = int.Parse(version);
-                Djenerative.Properties.Preset.Default.Save();
-
-                ProcessStartInfo Info = new ProcessStartInfo();
-                Info.Arguments = $"/C choice /C Y /N /D Y /T 3 & Del \"\"\"{exe}\"\"\" & TIMEOUT /T 1 & Ren \"\"\"{tmp}\"\"\" \"\"\"{exe}\"\"\" & TIMEOUT /T 1 & \"\"\"{fullPath}\"\"\"";
-                Info.WindowStyle = ProcessWindowStyle.Hidden;
-                Info.CreateNoWindow = true;
-                Info.FileName = "cmd.exe";
-                Process.Start(Info);
-
-                Environment.FailFast("Updating");
+                Update(version);
             }
             catch (Exception ex)
             {
@@ -58,11 +74,52 @@ namespace Djenerative
             }
         }
 
+        private void Update(string version)
+        {
+            var messageBox = new MessageBoxModel
+            {
+                Text = "We are going to update Djenerative.",
+                Caption = "Update Available",
+                Icon = MessageBoxImage.Information,
+                Buttons = MessageBoxButtons.OkCancel(),
+            };
+
+            MessageBox.Show(messageBox);
+            if (messageBox.Result == MessageBoxResult.Cancel) Environment.FailFast("Neglected Update");
+
+            string download = Path.Combine(Environment.CurrentDirectory, "update.tmp");
+
+            var data = _client.GetByteArrayAsync(DownloadLink).Result;
+            File.WriteAllBytesAsync(download, data);
+
+            var process = Process.GetCurrentProcess();
+            string fullPath = process.MainModule!.FileName!;
+            string exe = Path.GetFileName(fullPath);
+            string tmp = Path.GetFileName(download);
+
+            Djenerative.Properties.Preset.Default.Version = int.Parse(version);
+            Djenerative.Properties.Preset.Default.Save();
+
+            ProcessStartInfo info = new()
+            {
+                Arguments = $"/C choice /C Y /N /D Y /T 3 & Del \"\"\"{exe}\"\"\" & Ren \"\"\"{tmp}\"\"\" \"\"\"{exe}\"\"\" & \"\"\"{fullPath}\"\"\" & pause",
+                WindowStyle = ProcessWindowStyle.Hidden,
+                CreateNoWindow = true,
+                FileName = "cmd.exe"
+            };
+            Process.Start(info);
+
+            Environment.Exit(0);
+        }
+
         private void Application_Startup(object sender, StartupEventArgs e)
         {
+
 #if DEBUG
-            return;
+            //return;
 #endif
+#pragma warning disable CS0162 // Unreachable code detected
+
             Process proc = Process.GetCurrentProcess();
             int count = Process.GetProcesses().Count(p => p.ProcessName == proc.ProcessName);
 
@@ -70,7 +127,7 @@ namespace Djenerative
             {
                 Environment.FailFast("Already Running");
             }
-            Update();
+            UpdateCheck();
         }
     }
 }
